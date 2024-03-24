32e824a7f51c1689014d5b7f2ed3d6fe9b46d230
Tasneem <tasneem.jameel9@gmail.com>
2024-03-24 23:27:47 +0300

    Get Charity Coordinates

--- Ghaith-backend/models/Charity.js
+++ Ghaith-backend/models/Charity.js
@@ -6,16 +6,8 @@ const charitySchema = new Schema(
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     logo: String,
     googlemaplink: String,
-    // location: {
-    //   type: {
-    //     type: String,
-    //     enum: ['Point'],
-    //     default: 'Point' // Set the default value to 'Point'
-    //   },
-    //   coordinates: {
-    //     type: [Number]
-    //   }
-    // },
+    latitude: Number,
+    longitude: Number,
     cr_number: String,
     donations: [{ type: Schema.Types.ObjectId, ref: 'Donation' }]
   },
