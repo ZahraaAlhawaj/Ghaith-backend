e5732ebc4d0648dc08b849d735ad638f46461ca5
Tasneem <tasneem.jameel9@gmail.com>
2024-03-27 20:50:02 +0300

    Return approved charities

--- Ghaith-backend/routes/charities.js
+++ Ghaith-backend/routes/charities.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const middleware = require('../middleware')
 
 const charitiesController = require('../controllers/charities')
 
 //find all Charity
 router.get('/', charitiesController.findAllCharities)
+
+//find approved Charity
+router.get('/approved', charitiesController.findApprovedCharities)
+
 //find Charity
 router.get('/:id', charitiesController.findCharity)
 
